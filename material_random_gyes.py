db7123ccee49948e22db8b046c02728ea80c07ee
kilon <thekilon@yahoo.co.uk>
2011-06-04 23:07:05 +0300

    buidling a main class "random_material" to organise the code abit more

--- material_random_gyes.py
+++ material_random_gyes.py
@@ -17,72 +17,74 @@ bl_info = {
 import bpy ,random
 from bpy.props import *
 
-def scn_props():
-    bpy.types.Scene.rp = IntProperty(name="percentage", description = "percentage of randomisation" , min = 0 , max = 100 , default = 50)
-    
-    bpy.types.Scene.rdiffuse_shader = BoolProperty(name= "Diffuse Shader" ,description = "Randomise Diffuse Shader" , default = True)
-    
-    bpy.types.Scene.rdiffuse_color = BoolProperty(name= "Diffuse Color" ,description = "Randomise Diffuse Color", default = True  )
-    
-    bpy.types.Scene.rdiffuse_intensity = BoolProperty(name= "Diffuse Intensity" ,description = "Randomise Diffuse Intensity" , default = True )
-    
-    bpy.types.Scene.rspecular_color = BoolProperty(name= "Specular Color" ,description = "Randomise Specular Color" , default = True)
-    
-    bpy.types.Scene.rspecular_shader = BoolProperty(name= "Specular Shader" ,description = "Randomise Specular Shader" , default = True)
-    
-    bpy.types.Scene.rspecular_intensity = BoolProperty(name= "Specular Intensity" ,description = "Randomise Specular Intensity" , default = True)
-    
-    bpy.types.Scene.rspecular_hardness = BoolProperty(name= "Specular Hardness" ,description = "Randomise Specular Hardness" , default = True)
-    
-    bpy.types.Scene.rtransparency = BoolProperty(name= "Transparency" ,description = "Use and Randomise Transparency" , default = True)
-    
-    
-scn_props()
 
-#function for randomising the material properties 
-def random_material(ob,name):
-    #mat = bpy.data.materials.new(name)
-    mat = ob
-    scn = bpy.context.scene
     
-    if scn.rdiffuse_color:#scn["rdiffuse_color"]==True:
-        mat.diffuse_color = (random.random(),random.random(),random.random())
+
+
+#function for randomising the material properties
+class random_material:
     
-    if scn.rdiffuse_shader:
-        mat.diffuse_shader = random.choice(['LAMBERT','FRESNEL','TOON','MINNAERT'])
+    def __init__(self):
+     
+        bpy.types.Scene.rp = IntProperty(name="percentage", description = "percentage of randomisation" , min = 0 , max = 100 , default = 50)
     
-    if scn.rdiffuse_intensity:
+        bpy.types.Scene.rdiffuse_shader = BoolProperty(name= "Diffuse Shader" ,description = "Randomise Diffuse Shader" , default = True)
+    
+        bpy.types.Scene.rdiffuse_color = BoolProperty(name= "Diffuse Color" ,description = "Randomise Diffuse Color", default = True  )
+    
+        bpy.types.Scene.rdiffuse_intensity = BoolProperty(name= "Diffuse Intensity" ,description = "Randomise Diffuse Intensity" , default = True )
+    
+        bpy.types.Scene.rspecular_color = BoolProperty(name= "Specular Color" ,description = "Randomise Specular Color" , default = True)
+    
+        bpy.types.Scene.rspecular_shader = BoolProperty(name= "Specular Shader" ,description = "Randomise Specular Shader" , default = True)
+    
+        bpy.types.Scene.rspecular_intensity = BoolProperty(name= "Specular Intensity" ,description = "Randomise Specular Intensity" , default = True)
+    
+        bpy.types.Scene.rspecular_hardness = BoolProperty(name= "Specular Hardness" ,description = "Randomise Specular Hardness" , default = True)
+    
+        bpy.types.Scene.rtransparency = BoolProperty(name= "Transparency" ,description = "Use and Randomise Transparency" , default = True)
+    
+     
+    def random_material(self,ob,name):
+        #mat = bpy.data.materials.new(name)
+        mat = ob
+        scn = bpy.context.scene
+    
+        if scn.rdiffuse_color:#scn["rdiffuse_color"]==True:
+            mat.diffuse_color = (random.random(),random.random(),random.random())
+    
+        if scn.rdiffuse_shader:
+            mat.diffuse_shader = random.choice(['LAMBERT','FRESNEL','TOON','MINNAERT'])
+    
+        if scn.rdiffuse_intensity:
             mat.diffuse_intensity = random.random()  # *(0.01*scn["rp"])
     
-    if scn.rspecular_color:
-        mat.specular_color = (random.random(),random.random(),random.random())
+        if scn.rspecular_color:
+            mat.specular_color = (random.random(),random.random(),random.random())
     
-    if scn.rspecular_shader:
-        mat.specular_shader = random.choice(['COOKTORR','WARDISO','TOON','BLINN','PHONG'])
+        if scn.rspecular_shader:
+            mat.specular_shader = random.choice(['COOKTORR','WARDISO','TOON','BLINN','PHONG'])
     
-    if scn.rspecular_intensity:
-        mat.specular_intensity = random.random()
+        if scn.rspecular_intensity:
+            mat.specular_intensity = random.random()
     
-    if scn.rspecular_hardness:
-        mat.specular_hardness = random.randrange(1,511,1)
+        if scn.rspecular_hardness:
+            mat.specular_hardness = random.randrange(1,511,1)
     
-    mat.use_transparency = scn.rtransparency # random.randrange(0,2)
+        mat.use_transparency = scn.rtransparency # random.randrange(0,2)
     
-    if mat.use_transparency == True :
-        mat.transparency_method == random.choice(['MASK', 'Z_TRANSPARENCY', 'RAYTRACE'])
-        if mat.transparency_method == 'MASK' :
-            mat.alpha = random.random()
-        if mat.transparency_method == 'Z_TRANSPARENCY' :
-            mat.alpha = random.random()
-            mat.specular_alpha= random.random()
+        if mat.use_transparency == True :
+            mat.transparency_method == random.choice(['MASK', 'Z_TRANSPARENCY', 'RAYTRACE'])
+            if mat.transparency_method == 'MASK' :
+                mat.alpha = random.random()
+            if mat.transparency_method == 'Z_TRANSPARENCY' :
+                mat.alpha = random.random()
+                mat.specular_alpha= random.random()
       
-    mat.ambient = random.random()
-    return mat
+        mat.ambient = random.random()
+        return mat
 
-#fuction for assinging the random material to an object 
-def setMaterial(ob, mat):
-    me = ob.data
-    me.materials.append(mat)
+rm = random_material()
     
 #this is the script's main loop 
 def main_loop(origin):
@@ -91,9 +93,7 @@ def main_loop(origin):
     for i in bpy.context.selected_objects :
         if i.type == 'MESH' :
             
-            #setMaterial(i, i.active_material)
-            
-            rm = random_material(i.active_material,'Random')
+            rm.random_material(i.active_material,'Random')
             
            
 # this the main panel
