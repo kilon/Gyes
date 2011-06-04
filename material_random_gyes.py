9d175dd36161d301615eb59bb18b323fa9161dcd
kilon <thekilon@yahoo.co.uk>
2011-06-04 11:34:03 +0300

    gui expanded , now randomisation can be focused

--- material_random_gyes.py
+++ material_random_gyes.py
@@ -15,21 +15,69 @@ bl_info = {
     
 
 import bpy ,random
+from bpy.props import *
+
+def scn_props():
+    bpy.types.Scene.rp = IntProperty(name="percentage", description = "percentage of randomisation" , min = 0 , max = 100)
+    bpy.context.scene["rp"] = 50
+    
+    bpy.types.Scene.rdiffuse_shader = BoolProperty(name= "Diffuse Shader" ,description = "Randomise Diffuse Shader" )
+    bpy.context.scene["rdiffuse_shader"] = True
+    
+    bpy.types.Scene.rdiffuse_color = BoolProperty(name= "Diffuse Color" ,description = "Randomise Diffuse Color" )
+    bpy.context.scene["rdiffuse_color"] = True
+    
+    bpy.types.Scene.rdiffuse_intensity = BoolProperty(name= "Diffuse Intensity" ,description = "Randomise Diffuse Intensity" )
+    bpy.context.scene["rdiffuse_intensity"] = True
+    
+    bpy.types.Scene.rspecular_color = BoolProperty(name= "Specular Color" ,description = "Randomise Specular Color" )
+    bpy.context.scene["rspecular_color"] = True
+    
+    bpy.types.Scene.rspecular_shader = BoolProperty(name= "Specular Shader" ,description = "Randomise Specular Shader" )
+    bpy.context.scene["rspecular_shader"] = True
+    
+    bpy.types.Scene.rspecular_intensity = BoolProperty(name= "Specular Intensity" ,description = "Randomise Specular Intensity" )
+    bpy.context.scene["rspecular_intensity"] = True
+    
+    bpy.types.Scene.rspecular_hardness = BoolProperty(name= "Specular Hardness" ,description = "Randomise Specular Hardness" )
+    bpy.context.scene["rspecular_hardness"] = True
+    
+    bpy.types.Scene.rtransparency = BoolProperty(name= "Transparency" ,description = "Use and Randomise Transparency" )
+    bpy.context.scene["rtransparency"] = True
+    
+    
+scn_props()
 
 #function for randomising the material properties 
 def random_material(ob,name):
     #mat = bpy.data.materials.new(name)
     mat = ob
-    mat.diffuse_color = (random.random(),random.random(),random.random())
-    mat.diffuse_shader = random.choice(['LAMBERT','FRESNEL','TOON','MINNAERT']) 
-    mat.diffuse_intensity = random.random()
-    mat.specular_color = (random.random(),random.random(),random.random())
-    mat.specular_shader = random.choice(['COOKTORR','WARDISO','TOON','BLINN','PHONG'])
-    mat.specular_intensity = random.random()
-    mat.specular_hardness = random.randrange(1,511,1)
-    mat.use_transparency = random.randrange(0,2)
+    scn = bpy.context.scene
     
-    if mat.use_transparency == 1 :
+    if scn["rdiffuse_color"]==True:
+        mat.diffuse_color = (random.random(),random.random(),random.random())
+    
+    if scn["rdiffuse_shader"]:
+        mat.diffuse_shader = random.choice(['LAMBERT','FRESNEL','TOON','MINNAERT'])
+    
+    if scn["rdiffuse_intensity"]:
+            mat.diffuse_intensity = random.random()  # *(0.01*scn["rp"])
+    
+    if scn["rspecular_color"]:
+        mat.specular_color = (random.random(),random.random(),random.random())
+    
+    if scn["rspecular_shader"]:
+        mat.specular_shader = random.choice(['COOKTORR','WARDISO','TOON','BLINN','PHONG'])
+    
+    if scn["rspecular_intensity"]:
+        mat.specular_intensity = random.random()
+    
+    if scn["rspecular_hardness"]:
+        mat.specular_hardness = random.randrange(1,511,1)
+    
+    mat.use_transparency = scn["rtransparency"] # random.randrange(0,2)
+    
+    if mat.use_transparency == True :
         mat.transparency_method == random.choice(['MASK', 'Z_TRANSPARENCY', 'RAYTRACE'])
         if mat.transparency_method == 'MASK' :
             mat.alpha = random.random()
@@ -63,12 +111,28 @@ class gyes_panel(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     
+    
+    
     @classmethod    
     def poll(self, context):
         if context.object and context.object.type == 'MESH':                    return len(context.object.data.materials)
         
     def draw(self, context):
-        self.layout.operator("gyes.random_material")
+        
+        layout = self.layout
+        layout.label(text="Options")
+        box = layout.box()
+        box.prop(context.scene,"rdiffuse_shader")
+        box.prop(context.scene,"rdiffuse_color")
+        box.prop(context.scene,"rdiffuse_intensity")
+        box.prop(context.scene,"rspecular_shader")
+        box.prop(context.scene,"rspecular_color")
+        box.prop(context.scene,"rspecular_intensity")
+        box.prop(context.scene,"rspecular_hardness")
+        box.prop(context.scene,"rtransparency")
+        
+        #box.prop(context.scene,"rp")
+        layout.operator("gyes.random_material")
 
 #this is the random material button
 class gyes_random_material(bpy.types.Operator):
