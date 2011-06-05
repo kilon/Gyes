4bc82e80692bade33969ccfec61981a8624676f2
kilon <thekilon@yahoo.co.uk>
2011-06-06 01:11:34 +0300

    history almost working , it has problems recalling correct settings

--- material_random_gyes.py
+++ material_random_gyes.py
@@ -32,7 +32,7 @@ class random_material_class:
         bpy.types.Scene.rspecular_hardness = BoolProperty(name= "Specular Hardness" ,description = "Randomise Specular Hardness" , default = True)
         bpy.types.Scene.rtransparency = BoolProperty(name= "Transparency" ,description = "Use and Randomise Transparency" , default = True)
         bpy.types.Scene.history_index = IntProperty(name= "History Index" ,description = "The Number of Random Material Assigned to the Active MAterial of the Selected Object from the history" , default = 0)
-        self.rm_history=[]
+        self.rm_history={}
         
         
     # the fuction that randomises the material 
@@ -44,46 +44,89 @@ class random_material_class:
         
         bpy.types.Scene.history_index = IntProperty(name= "History Index" ,description = "The Number of Random Material Assigned to the Active MAterial of the Selected Object from the history" , default = 0, min = 0,max = len(self.rm_history))
         
-        self.rm_history.append(mat)
-        scn.history_index = len(self.rm_history)
+        #self.rm_history.append(mat)
+        scn.history_index = len(self.rm_history)+1
     
         if scn.rdiffuse_color:#scn["rdiffuse_color"]==True:
             mat.diffuse_color = (random.random(),random.random(),random.random())
+            
     
         if scn.rdiffuse_shader:
             mat.diffuse_shader = random.choice(['LAMBERT','FRESNEL','TOON','MINNAERT'])
+            
+            
     
         if scn.rdiffuse_intensity:
             mat.diffuse_intensity = random.random()  # *(0.01*scn["rp"])
+            
     
         if scn.rspecular_color:
             mat.specular_color = (random.random(),random.random(),random.random())
+            
     
         if scn.rspecular_shader:
             mat.specular_shader = random.choice(['COOKTORR','WARDISO','TOON','BLINN','PHONG'])
+            
     
         if scn.rspecular_intensity:
             mat.specular_intensity = random.random()
+            
     
         if scn.rspecular_hardness:
             mat.specular_hardness = random.randrange(1,511,1)
+            
     
         mat.use_transparency = scn.rtransparency # random.randrange(0,2)
+        
     
         if mat.use_transparency == True :
             mat.transparency_method == random.choice(['MASK', 'Z_TRANSPARENCY', 'RAYTRACE'])
+            
             if mat.transparency_method == 'MASK' :
                 mat.alpha = random.random()
+                
             if mat.transparency_method == 'Z_TRANSPARENCY' :
                 mat.alpha = random.random()
+                
                 mat.specular_alpha= random.random()
+                
+               
       
         mat.ambient = random.random()
+        self.rm_history[scn.history_index]= {"diffuse_color" : mat.diffuse_color,
+          "diffuse_shader" : mat.diffuse_shader , 
+          "diffuse_intensity" : mat.diffuse_intensity ,
+          "specular_color" : mat.specular_color , 
+          "specular_shader" : mat.specular_shader ,
+          "specular_intensity" : mat.specular_intensity , 
+          "specular_hardness" : mat.specular_hardness , 
+          "use_transparency" : mat.use_transparency , 
+          "transparency_method" : mat.transparency_method , 
+          "alpha" : mat.alpha , 
+          "specular_alpha" : mat.specular_alpha , 
+          "ambient" : mat.ambient }
+          
         return mat
     
+    
     def set_material(self):
-       print("aha")
-       bpy.context.selected_objects[0].active_material=self.rm_history[bpy.context.scene.history_index]
+        
+        scn = bpy.context.scene
+        mat = bpy.context.selected_objects[0].active_material
+        
+        mat.diffuse_color = self.rm_history[scn.history_index]["diffuse_color"]
+        mat.diffuse_shader = self.rm_history[scn.history_index]["diffuse_shader"]
+        mat.diffuse_intensity = self.rm_history[scn.history_index]["diffuse_intensity"]
+        mat.specular_color = self.rm_history[scn.history_index]["specular_color"]
+        mat.specular_shader = self.rm_history[scn.history_index]["specular_shader"]
+        mat.specular_intensity = self.rm_history[scn.history_index]["specular_intensity"]
+        mat.specular_hardness = self.rm_history[scn.history_index]["specular_hardness"]
+        mat.use_transparency = self.rm_history[scn.history_index]["use_transparency"]
+        mat.transparency_method = self.rm_history[scn.history_index]["transparency_method"]
+        mat.alpha = self.rm_history[scn.history_index]["alpha"]
+        mat.specular_alpha = self.rm_history[scn.history_index]["specular_alpha"]
+        mat.ambient = self.rm_history[scn.history_index]["ambient"]
+       
     
     
 rm = random_material_class()
@@ -96,15 +139,12 @@ def main_loop(origin):
         if i.type == 'MESH' :
             
             rm.random_material(i.active_material,'Random')
-            
-           
+                   
 # this the main panel
 class gyes_panel(bpy.types.Panel):
     bl_label = "Gyes"
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
-    
-    
     
     @classmethod    
     def poll(self, context):
@@ -124,8 +164,8 @@ class gyes_panel(bpy.types.Panel):
         box.prop(context.scene,"rspecular_hardness")
         box.prop(context.scene,"rtransparency")
         
-        #box.prop(context.scene,"rp")
         layout.operator("gyes.random_material")
+        
         layout.label(text="History")
         history_box= layout.box()
         history_box.prop(context.scene, "history_index")
