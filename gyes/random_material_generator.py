b126538d70cd1ee884b565f45a362bf331665678
thekilon <thekilon@yahoo.co.uk>
2011-07-27 13:30:56 +0300

    try to fix stuff

--- gyes/random_material_generator.py
+++ gyes/random_material_generator.py
@@ -1,37 +1,3 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-
-
-""" Copyright  Kilon 2011 GPL licence applies"""
-
-bl_info = {
-    "name": "Material_Gyes",
-    "description": "Random Material Generator",
-    "author": "Kilon",
-    "version": (0,1),
-    "blender": (2, 5, 7),
-    "api": 36147,
-    "location": "View3D > Left panel ",
-    "warning": '', # used for warning icon and text in addons panel
-    "wiki_url": "http://blenderartists.org/forum/showthread.php?218110-Gyes-%28Random-Material-Generator%29",
-    "tracker_url": "http://projects.blender.org/tracker/index.php?func=detail&aid=27470&group_id=153&atid=467",
-    "category": "Other"}
-    
 # first we import all the required modules
 import bpy ,random , copy 
 from bpy.props import *
