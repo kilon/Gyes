1318100914e6cce48d2694c81c1b66b54fbb0aac
thekilon <thekilon@yahoo.co.uk>
2011-11-01 10:12:52 +0200

    removed any unecessary files

--- gyes/reload.py
+++
@@ -1,8 +0,0 @@
-# This is a helper module. 
-# it allows me to see changes I make in source code without reopening blender
-# open it in text editor and just click run script everytime gyes source code
-# is changed
-
-import gyes
-import imp
-imp.reload(gyes)
